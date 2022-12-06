da9354249cbbfa5193cc9643d6745a2cad893c7a
e190740 <j.bouffioux@student.helmo.be>
2022-12-06 13:57:03 +0100

    Refactored a few things to add clarity and added the README for the 6th day

--- day06/solution.py
+++ day06/solution.py
@@ -1,6 +1,8 @@
 def read_input():
-    with open('../day06/test.txt') as f:
-        return f.readlines()
+    with open('../day06/input.txt') as f:
+        array = f.readlines()
+    f.close()
+    return array
 
 
 def string_has_duplicates(string):
@@ -10,15 +12,14 @@ def string_has_duplicates(string):
     return False
 
 
-def get_position_of_first_signal(size):
+def get_last_position_of_first_signal(size):
     first_signals = []
-    input_file = read_input()
-    for line in input_file:
+    for line in read_input():
         line = line.strip()
-        if line != '\n' and len(line) > 3:
+        if line != '\n' and len(line) >= size:
             signal = line[:size]
             i = 0
-            while string_has_duplicates(signal):
+            while string_has_duplicates(signal) and i + size < len(line):
                 i += 1
                 signal = line[i:size + i]
             first_signals.append(f'{i + size}')
@@ -26,13 +27,8 @@ def get_position_of_first_signal(size):
 
 
 def part1():
-    return get_position_of_first_signal(4)
+    return get_last_position_of_first_signal(4)
 
 
 def part2():
-    return get_position_of_first_signal(14)
-
-
-print(part1())
-
-print(part2())
+    return get_last_position_of_first_signal(14)
